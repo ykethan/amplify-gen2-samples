029c9bc8de2852356863feb8b5078fde1d507da2
ykethan <ykethan@amazon.com>
2024-11-14 19:32:43 -0500

    recepie stuff

--- amplify-gen2-ai-kit/src/App.tsx
+++ amplify-gen2-ai-kit/src/App.tsx
@@ -1,10 +1,12 @@
 import {
+  Authenticator,
   Button,
   Flex,
   Loader,
   TextAreaField,
   View,
 } from "@aws-amplify/ui-react";
+
 import "@aws-amplify/ui-react/styles.css";
 import * as React from "react";
 import ReactMarkdown from "react-markdown";
@@ -20,35 +22,37 @@ export default function App() {
   };
 
   return (
-    <Flex direction="column">
-      <Flex direction="row">
-        <TextAreaField
-          autoResize
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          label="Description"
-        />
-        <Button onClick={handleClick}>Generate recipe</Button>
+    <Authenticator>
+      <Flex direction="column">
+        <Flex direction="row">
+          <TextAreaField
+            autoResize
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            label="Description"
+          />
+          <Button onClick={handleClick}>Generate recipe</Button>
+        </Flex>
+        {isLoading ? (
+          <Loader variation="linear" />
+        ) : (
+          <View className="recipe-content">
+            {data?.name && <ReactMarkdown>{`# ${data.name}`}</ReactMarkdown>}
+            {data?.ingredients && (
+              <ReactMarkdown>
+                {`## Ingredients\n${data.ingredients
+                  .map((ingredient) => `- ${ingredient}`)
+                  .join("\n")}`}
+              </ReactMarkdown>
+            )}
+            {data?.instructions && (
+              <ReactMarkdown>
+                {`## Instructions\n${data.instructions}`}
+              </ReactMarkdown>
+            )}
+          </View>
+        )}
       </Flex>
-      {isLoading ? (
-        <Loader variation="linear" />
-      ) : (
-        <View className="recipe-content">
-          {data?.name && <ReactMarkdown>{`# ${data.name}`}</ReactMarkdown>}
-          {data?.ingredients && (
-            <ReactMarkdown>
-              {`## Ingredients\n${data.ingredients
-                .map((ingredient) => `- ${ingredient}`)
-                .join("\n")}`}
-            </ReactMarkdown>
-          )}
-          {data?.instructions && (
-            <ReactMarkdown>
-              {`## Instructions\n${data.instructions}`}
-            </ReactMarkdown>
-          )}
-        </View>
-      )}
-    </Flex>
+    </Authenticator>
   );
 }
