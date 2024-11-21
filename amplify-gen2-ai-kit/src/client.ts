bfe2a7c1778976d57f379bd6680126de536e8d37
ykethan <ykethan@amazon.com>
2024-11-20 23:03:33 -0500

    remove app

--- amplify-gen2-ai-kit/src/client.ts
+++
@@ -1,6 +0,0 @@
-import { createAIHooks } from "@aws-amplify/ui-react-ai";
-import { generateClient } from "aws-amplify/api";
-import { Schema } from "../amplify/data/resource";
-
-export const client = generateClient<Schema>({ authMode: "userPool" });
-export const { useAIConversation, useAIGeneration } = createAIHooks(client);
